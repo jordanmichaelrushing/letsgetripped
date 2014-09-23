7fc36e98fbaae56949e51bd097b43846e56dfc08
JordanRushing <Jordan@RushingFitness.com>
2014-09-22 22:16:19 -0400

    change bool to string

--- db/migrate/20140922211453_create_calf_crunches.rb
+++ db/migrate/20140922211453_create_calf_crunches.rb
@@ -1,7 +1,7 @@
 class CreateCalfCrunches < ActiveRecord::Migration
   def change
     create_table :calf_crunches do |t|
-      t.boolean :set_type, default: "Crunch"
+      t.string :set_type, default: "Crunch"
       t.integer :set, default: 0
       t.integer :reps, default: 0
       t.integer :day_id
