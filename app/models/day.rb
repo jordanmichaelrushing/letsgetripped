aae63dea08b1a39529d4f6a1d5402d1a27a1bc77
JordanRushing <Jordan@RushingFitness.com>
2014-10-02 09:22:32 -0400

    Add in more analytics for cardio and bring back exercises

--- app/models/day.rb
+++ app/models/day.rb
@@ -16,9 +16,15 @@ class Day < ActiveRecord::Base
 
   def self.get_weekly_stats(current_user)
     ctr = 0
+    cardio = current_user.days
+          .select("WEEK(days.date,1) as week, COUNT(exercises.id) as exercise_total, COUNT(cardios.id) as cardio_total, COUNT(cardios.id) as cardio_total, ROUND(COALESCE((SUM(cardios.distance)/COUNT(cardios.id)),0),2) as avg_cardio_dist, ROUND(COALESCE((SUM(cardios.distance)/COUNT(cardios.id) * 3),0),2) as avg_cardio_speed")
+          .joins("LEFT JOIN cardios on cardios.user_id = #{current_user.id} AND cardios.day_id = days.id")
+          .joins("LEFT JOIN exercises on exercises.user_id = #{current_user.id} AND exercises.day_id = days.id")
+          .group("WEEK(days.date,1)")
+          .order("WEEK(days.date,1)")
+
     x = current_user.days
-          .select("days.date as date, COUNT(DISTINCT(cardios.id)) as cardio_total,COUNT(DISTINCT(meals.id)) as meal_total,ROUND(COALESCE(SUM((meals.protein * 4)+(meals.fats * 9)+(meals.carbs * 4)),0),2) as calories,super_challenges.id AS sc_id, previous_sc.id AS previous_sc_id, super_challenges.date AS sc_date, previous_sc.date AS previous_sc_date,super_challenges.duration AS duration,super_challenges.push_ups AS push_ups, super_challenges.pull_ups AS pull_ups, previous_sc.duration AS previous_sc_duration, previous_sc.push_ups AS previous_sc_push_ups,previous_sc.pull_ups AS previous_sc_pull_ups")
-          .joins("LEFT JOIN cardios on cardios.day_id = days.id")
+          .select("days.date as date,COUNT(DISTINCT(meals.id)) as meal_total,ROUND(COALESCE(SUM((meals.protein * 4)+(meals.fats * 9)+(meals.carbs * 4)),0),2) as calories,super_challenges.id AS sc_id, previous_sc.id AS previous_sc_id, super_challenges.date AS sc_date, previous_sc.date AS previous_sc_date,super_challenges.duration AS duration,super_challenges.push_ups AS push_ups, super_challenges.pull_ups AS pull_ups, previous_sc.duration AS previous_sc_duration, previous_sc.push_ups AS previous_sc_push_ups,previous_sc.pull_ups AS previous_sc_pull_ups")
           .joins("LEFT JOIN meals on meals.day_id = days.id")
           .joins("LEFT OUTER JOIN super_challenges ON super_challenges.user_id = #{current_user.id} AND WEEK(super_challenges.date,1) = WEEK(days.date,1)")
           .joins("LEFT OUTER JOIN super_challenges AS previous_sc ON previous_sc.user_id = #{current_user.id} AND WEEK(previous_sc.date,1) = (WEEK(days.date,1) - 1)")
@@ -44,7 +50,9 @@ class Day < ActiveRecord::Base
       else
         string +="<li><i>Super Challenge Not Completed This Week</i></li>"
       end
-      string +="<li>Total Cardios Done: #{f.cardio_total}</li></ul></ul>"
+
+      string +="<li>Total Exercises Done: #{cardio[ctr-1].exercise_total}</li><li>Total Cardios Done: #{cardio[ctr-1].cardio_total}</li>
+                <li>Average Cardio Distance: #{cardio[ctr-1].avg_cardio_dist} Miles</li><li>Average Cardio Speed: #{cardio[ctr-1].avg_cardio_speed} mph</li></ul></ul>"
       string.html_safe
     end
   end
